520e997060823d920620baaafec6a208e04b1ab9
Nortey Daniel Yeboah <gogamerdan247@gmail.com>
2026-02-14 12:46:18 +0000

    work

--- test2.py
+++ test2.py
@@ -1,23 +1,53 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from scipy.linalg import lu
 
-points = np.array([
-    [0, 0],
-    [1, 0],
-    [1, 1],
-    [0, 1]
-])
+# Define matrix A
+A = np.array([
+    [2, -1, 1],
+    [3, 3, 9],
+    [3, 3, 5]
+], dtype=float)
 
-rotation_matrix = np.array([
-    [0, -1],
-    [1,  0]
-])
+# Define vector b
+b = np.array([2, -1, 4], dtype=float)
 
-rotated_points = points @ rotation_matrix
+print("Matrix A:")
+print(A)
 
-plt.plot(points[:,0], points[:,1], 'o-', label='Original')
-plt.plot(rotated_points[:,0], rotated_points[:,1], 'o-', label='Rotated')
-plt.legend()
-plt.title("Rotation using a Matrix")
-plt.axis('equal')
-plt.show()
+print("\nVector b:")
+print(b)
+
+# Perform LU Decomposition (PA = LU)
+P, L, U = lu(A)
+
+print("\nPermutation Matrix P:")
+print(P)
+
+print("\nLower Triangular Matrix L:")
+print(L)
+
+print("\nUpper Triangular Matrix U:")
+print(U)
+
+# Verify PA = LU
+print("\nVerification: PA")
+print(np.dot(P, A))
+
+print("\nVerification: LU")
+print(np.dot(L, U))
+
+# Solve Ax = b using LU
+
+# Step 1: Solve Ly = Pb
+Pb = np.dot(P, b)
+y = np.linalg.solve(L, Pb)
+
+# Step 2: Solve Ux = y
+x = np.linalg.solve(U, y)
+
+print("\nSolution vector x:")
+print(x)
+
+# Final verification Ax = b
+print("\nVerification Ax:")
+print(np.dot(A, x))
